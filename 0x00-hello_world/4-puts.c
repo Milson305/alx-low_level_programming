dd42737b31b81c83becb94fb85d5d1420d4546ce
Edmilson <milson305@gmail.com>
2023-02-18 18:23:59 +0200

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /**
 *main - prints "Programming is like building a multilingual puzzle,
 *followed by a newline"
-*Return : Always 0.
+*Return : Always 0 (Success)
 */
-int main(void) {
+int main(void) 
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
