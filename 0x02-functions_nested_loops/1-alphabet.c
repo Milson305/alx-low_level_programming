6d57a1b72749b03e11361de9f5b25d45f0066004
Edmilson <milson305@gmail.com>
2023-02-21 18:08:01 +0200

    0_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Write a function that prints 10 times the alphabet, in lowercase,
+*followed by a new line
+*Return : Always return (0)
+*/
+void print_alphabet(void)
+{
+printf("a /n")
+return (0);
+}
