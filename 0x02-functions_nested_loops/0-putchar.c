553c4bcbfb77e4b3556887a879af273db255f3c2
Edmilson <milson305@gmail.com>
2023-02-22 19:47:54 +0200

    7H00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
+/**
+* main - print _putchar
+* is written by mbah
+* Return: 0
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,4 +17,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
