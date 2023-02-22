694f2912ec8092fd292a540a8501a224d4ef2ce6
Edmilson <milson305@gmail.com>
2023-02-22 21:26:26 +0200

    Isalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Succes)
- */
+*print_alphabet - a function that prints the alphabet , in lowercase
+*Return : Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	int n;
